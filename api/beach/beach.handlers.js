e89a5043129d58768b2fee6556f88d7d6d835dc7
Dai <dai.williams@bbc.co.uk>
2015-06-25 22:45:00 +0100

    updated handlers to only return specified fields

--- api/beach/beach.handlers.js
+++ api/beach/beach.handlers.js
@@ -5,7 +5,7 @@ var BeachModel = require('./beach.model');
 exports.suggestBeach = function *() {
 
   let search = new RegExp(this.query.q, 'i');
-  let query = BeachModel.find({name: search});
+  let query = BeachModel.find({name: search}, 'id name country');
 
   this.body = yield query.exec();
 
