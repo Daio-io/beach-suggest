e8aec2ab8d032c13bc2109897fcaa11335f972c1
Dai <dai.williams@bbc.co.uk>
2015-06-25 22:37:33 +0100

    created index

---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var http = require('http');
+var koa = require('koa');
+var app = koa();
+var settings = require('./api/conf/api.settings');
+
+// Bootstrapping
+require('./api/conf/api.bootstrap')();
+
+// Routes
+require('./api/conf/api.routes')(app);
+
+var server = http.createServer(app.callback());
+
+server.listen(settings.port, function() {
+
+  console.log('Surf-Query Started on:', settings.port);
+
+});
