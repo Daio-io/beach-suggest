b159d7186016eb8fd654e5a75acfef48f5dce7c1
Dai <dai.williams@bbc.co.uk>
2015-06-30 23:24:44 +0100

    limited search query to 50 characters

--- api/beach/beach.handlers.js
+++ api/beach/beach.handlers.js
@@ -9,7 +9,10 @@ exports.suggestBeach = function *() {
   if (tools.existy(this.query.q)) {
 
     let safeSearch = tools.cleansey(this.query.q);
-    let search = new RegExp(safeSearch, 'i');
+    let search = new RegExp(
+      tools.clippy(safeSearch, 50), 'i'
+    );
+    
     let query = BeachModel.find({name: search}, 'id name country');
 
     this.body = yield query.exec();
