aebc248a484e92ae57c491a3dc7c37dc36353676
Daveloper87 <dai.williams@bbc.co.uk>
2015-07-02 21:05:39 +0100

    confiured caching

--- api/conf/db/db.setup.js
+++ api/conf/db/db.setup.js
@@ -5,17 +5,15 @@ var config = require('./db.config');
 var cache = require('mongoose-cache');
 
 module.exports = function() {
-
-  cache.install(mongoose, config.cache);
   
   switch (process.env.NODE_ENV) {
     case 'live':
-
+      cache.install(mongoose, config.prod.cache);
       mongoose.connect(config.prod.connectionString, config.prod.options);
       break;
 
     default:
-
+      cache.install(mongoose, config.dev.cache);
       mongoose.connect(config.dev.connectionString, config.dev.options);
       break;
 
